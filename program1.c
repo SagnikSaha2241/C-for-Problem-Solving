3e70ce1b2dcbacbec04dfb74b3705cb1eac43a1e
SagnikSaha2241 <sagniksaha843@gmail.com>
2025-03-05 11:22:44 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+int fact(int x)
+{
+	int fact=1,i;
+	
+	for(i=2;i<=x;i++)
+	{
+		fact=fact*i;
+	}
+	return fact;
+}
+
+
+int main()
+{
+	int n,r,p,i,res=0;
+	printf("Enter The values of n,r,p: ");
+	scanf("%d %d %d",&n ,&r, &p);
+	for(i=r;i<=p;i++)
+	{
+		res=res+(fact(n)/fact(n-i));
+	}
+	printf("%d",res);
+	
+	return 0;
+}
